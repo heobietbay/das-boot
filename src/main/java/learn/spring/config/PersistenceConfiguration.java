feff0a1517df3c16579150dc216588c7fbdcf02b
Khoa Tran <trandangkhoa@tma.com.vn>
2016-07-14 22:10:22 +0700

    Add PersistenceConfiguration

---
+++ src/main/java/learn/spring/config/PersistenceConfiguration.java
@@ -0,0 +1,33 @@
+package learn.spring.config;
+
+import javax.sql.DataSource;
+
+import org.springframework.boot.autoconfigure.flyway.FlywayDataSource;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceBuilder;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+
+/**
+ * Created by khoa on 7/13/2016.
+ */
+@Configuration
+public class PersistenceConfiguration {
+
+    @Bean
+    @ConfigurationProperties(prefix = "spring.datasource")
+    @Primary
+    public DataSource dataSource()
+    {
+        return DataSourceBuilder.create().build();
+    }
+
+    @Bean
+    @ConfigurationProperties(prefix = "datasource.flyway")
+    @FlywayDataSource
+     public DataSource flywayDataSource()
+    {
+        return DataSourceBuilder.create().build();
+    }
+}
